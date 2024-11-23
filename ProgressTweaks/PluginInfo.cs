0876ec423a50be9507031801beb491b759437c16
super-miner <theredmugproductions@gmail.com>
2024-11-22 23:10:31 -0800

    Updated plugin info

--- ProgressTweaks/PluginInfo.cs
+++ ProgressTweaks/PluginInfo.cs
@@ -11,9 +11,9 @@ using ProgressTweaks;
 
 namespace ProgressTweaks {
     internal static class PluginInfo {
-        public const string PLUGIN_NAME = "BepInEx Plugin";
+        public const string PLUGIN_NAME = "Progress Tweaks";
         
-        public const string PLUGIN_ID = "org.bepinex.my.plugin";
+        public const string PLUGIN_ID = "com.flowndev.progresstweaks";
 
         public const string PLUGIN_VERSION = "1.0.0";
     }
